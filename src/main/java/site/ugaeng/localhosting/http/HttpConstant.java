013621fa365d3a4cc9d08d53020ee76dcc36facd
60jong <rudwhd515@gmail.com>
2024-01-09 21:53:13 +0900

    const : Http Message의 상수 구분점들 생성

---
+++ src/main/java/site/ugaeng/localhosting/http/HttpConstant.java
@@ -0,0 +1,12 @@
+package site.ugaeng.localhosting.http;
+
+public class HttpConstant {
+
+    public static final String CRLF = "\r\n";
+
+    public static final String SEMI_COLON = ";";
+    public static final String COLON = ":";
+
+    public static final String SP = " ";
+    public static final String EMPTY = "";
+}
