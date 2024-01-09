ffdb8b07e378e342e12ce42f7235ffa7344aec94
60jong <rudwhd515@gmail.com>
2024-01-09 14:00:29 +0900

    fix : 프로젝트 세팅 수정

---
+++ script/buildAndRunExecutable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# change directory
+cd ..
+
+# build jar
+echo -e '### BUILD localhosting.jar start ###\n'
+./gradlew jar
+echo -e '\n### BUILD localhosting.jar success ###\n'
+
+# build localhosting.exe
+echo -e '\n### BUILD localhosting.exe start ###\n'
+launch4jc ./executable/config.xml
+echo -e '\n### BUILD localhosting.exe success ###\n'
+
+echo -e '\n### RUN localhosting.exe ###'
+./executable/localhosting.exe 9000
+
+read -p "Press enter to continue"
