037a6f23567bf2eb3b7fd651b6b06e2f64b9c7ff
60jong <rudwhd515@gmail.com>
2024-01-15 18:06:25 +0900

    fix : Tunnel 등록시에 사용(not spring container)하기 위해 스프링 빈이 아닌 싱글톤 객체로 수정

--- localhosting-server/src/main/java/site/ugaeng/localhostingserver/forward/TunnelClientRepository.java
+++ localhosting-server/src/main/java/site/ugaeng/localhostingserver/forward/TunnelClientRepository.java
@@ -1,30 +1,55 @@
-package site.ugaeng.localhostingserver.route;
+package site.ugaeng.localhostingserver.forward;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 import site.ugaeng.localhostingserver.tunnel.TunnelClient;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
-@Repository
 public class TunnelClientRepository {
+
+    private static TunnelClientRepository instance;
 
     private final Map<String, TunnelClient> connectionMap;
 
-    public TunnelClientRepository() {
+    private TunnelClientRepository() {
         connectionMap = new HashMap<>();
+    }
+
+    public static TunnelClientRepository getInstance() {
+        if (instance == null) {
+            instance = new TunnelClientRepository();
+            return instance;
+        }
+        return instance;
     }
 
     public void save(String domainName, TunnelClient connection) {
         connectionMap.put(domainName, connection);
     }
 
-    public TunnelClient find(String domainName) {
-        TunnelClient connection = connectionMap.get(domainName);
-        log.info("find connection : {}", connection);
+    public Optional<TunnelClient> find(String name) {
+        if (connectionMap.containsKey(name)) {
+            TunnelClient connection = connectionMap.get(name);
+            log.info("find connection : {}", connection);
+            return Optional.ofNullable(connection);
+        }
+        return Optional.empty();
+    }
 
-        return connection;
+    public int removeClosed() {
+        int removes = 0;
+
+        for (var entry : connectionMap.entrySet()) {
+            TunnelClient client = entry.getValue();
+            if (client.isClosed()) {
+                connectionMap.remove(entry.getKey());
+                removes++;
+            }
+        }
+
+        return removes;
     }
 }
