6e33a2779baef3f735a1dd25ba9ba28c2eae8d21
60jong <rudwhd515@gmail.com>
2024-01-17 04:28:24 +0900

    feat : HostController 터널 이름으로 요청을 forward시킨뒤 response outputstream에 결과 write한다.

--- localhosting-server/src/main/java/site/ugaeng/localhostingserver/host/HostController.java
+++ localhosting-server/src/main/java/site/ugaeng/localhostingserver/host/HostController.java
@@ -1,40 +1,61 @@
 package site.ugaeng.localhostingserver.host;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import site.ugaeng.localhostingserver.TunnelClientRepository;
+import site.ugaeng.localhostingserver.http.Response;
+import site.ugaeng.localhostingserver.forward.RequestForwarder;
 import site.ugaeng.localhostingserver.http.Request;
-import site.ugaeng.localhostingserver.tunnel.repository.TunnelRepository;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
 
 @RequiredArgsConstructor
 @RestController
 public class HostController {
 
-    private final TunnelRepository tunnelRepository;
-    private final TunnelClientRepository tunnelClientRepository;
-    private ObjectMapper om = new ObjectMapper();
-    @RequestMapping("/{tunnelDomainName}/host")
-    public String forwardRequest(@PathVariable String tunnelDomainName,
-                                  HttpServletRequest httpRequest) throws IOException {
+    private final RequestForwarder forwarder;
 
+    @RequestMapping("/{tunnelName}/host/**")
+    public void forwardRequest(@PathVariable String tunnelName,
+                               HttpServletRequest httpRequest,
+                               HttpServletResponse httpResponse) throws IOException
+    {
         Request request = Request.readFromHttpServletRequest(httpRequest);
-        Socket client = tunnelClientRepository.find(tunnelDomainName);
 
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-        String s = om.writeValueAsString(request);
-        System.out.println(s);
-        writer.write(s);
-        writer.flush();
+        Response response = forwarder.forwardRequestForTunnel(tunnelName, request);
 
-        return "SUCCESS";
+        writeResponse(response, httpResponse);
+    }
+
+    private void writeResponse(Response response, HttpServletResponse httpResponse) throws IOException {
+        // statusCode
+        reflectStatusLine(response, httpResponse);
+
+        // header
+        reflectHeaders(response, httpResponse);
+
+        // entity
+        writeEntity(response, httpResponse);
+    }
+
+    private static void reflectStatusLine(Response response, HttpServletResponse httpResponse) {
+        final int statusCode = response.getStatusLine().getStatusCode();
+        httpResponse.setStatus(statusCode);
+    }
+
+    private static void reflectHeaders(Response response, HttpServletResponse httpResponse) {
+        response.getHeaders()
+                .entrySet()
+                .stream()
+                .forEach(entry -> httpResponse.setHeader(entry.getKey(), entry.getValue()));
+    }
+
+    private static void writeEntity(Response response, HttpServletResponse httpResponse) throws IOException {
+        final String entity = response.getEntity();
+        httpResponse.getWriter()
+                .write(entity);
     }
 }
