47d6b0943ed8c936b6f77badb4fba775c8392fe8
60jong <rudwhd515@gmail.com>
2024-01-23 14:34:40 +0900

    feat : Tunnel 엔티티 생성

---
+++ localhosting-server/src/main/java/site/ugaeng/localhostingserver/tunnel/Address.java
@@ -0,0 +1,16 @@
+package site.ugaeng.localhostingserver.tunnel;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Address {
+
+    private String ip;
+    private int port;
+}
---
+++ localhosting-server/src/main/java/site/ugaeng/localhostingserver/tunnel/Tunnel.java
@@ -0,0 +1,25 @@
+package site.ugaeng.localhostingserver.tunnel;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Tunnel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address clientAddr;
+
+    public Tunnel(String name, Address clientAddr) {
+        this.name = name;
+        this.clientAddr = clientAddr;
+    }
+}
